0afb0f14d1d423620430370a4abd0df3c4291bb6
pennivise228 <grigoriy1608@gmail.com>
2023-06-06 13:47:08 +0400

    The program should print first "n" Fibonacci numbers

--- main.cpp
+++ main.cpp
@@ -1,16 +1,28 @@
-#include <iostream> 
+#include<iostream>
 using namespace std;
 
-int f(int n)
-{
-    if (n == 1 || n == 2) return (n - 1);
-    return f(n - 1) + f(n - 2);
+int fib(int n) {
+	if (n == 0) return 0;
+	if (n == 1) return 1;
+	return f(n - 1) + f(n - 2);
+	int x0 = 0;
+	int x1 = 1;
+	cout << x0 << endl;
+	if (n >= 1) cout << x1 << endl;
+	for (int i = 2; i <= n; i++) {
+		if (x0 < x1) {
+			x0 += x1;
+			cout << x0 << endl;
+		}
+		else {
+			x1 += x0;
+			cout << x1 << endl;
+		}
+	}
 }
 
-int main()
-{
-    int n;
-    cout << "n="; cin >> n;
-    cout << f(n) << " ";
-    cout << endl;
+int main() {
+	int n;
+	cin >> n;
+	cout << fib(n);
 }
