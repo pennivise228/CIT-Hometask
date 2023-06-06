8b2ef3258ae6ffbdafb20a51f07f9ccb5b9e2ac2
pennivise228 <grigoriy1608@gmail.com>
2023-06-06 13:43:15 +0400

    Recursively calculate "n"- Fibonacci number, provided that the sequence

--- main.cpp
+++ main.cpp
@@ -1,7 +1,32 @@
-#include<iostream>
-
+#include <iostream> 
 using namespace std;
 
-int main() {
-	cout << "Hello world!";
+
+int fib(int i)
+{
+    int f1 = 0;
+    int f2 = 1;
+    int fn;
+    if (i < 1) { return 0; }
+    if (i == 1) { cout << "0 1\n"; }
+    if (i > 1)
+    {
+        cout << "0 1 ";
+        for (int j = 1; j < i; j++)
+        {
+            fn = f1 + f2;
+            cout << fn << " ";
+            f1 = f2;
+            f2 = fn;
+        }
+    }
+};
+
+int main()
+{
+    cout << "Hello world";
+    int n;
+    cout << "Enter a number n = "; cin >> n;
+    cout << fib(n) << " ";
+    cout << endl;
 }
