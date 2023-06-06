90a875130bd73a8dde017e71031a2216edbe55b2
pennivise228 <grigoriy1608@gmail.com>
2023-06-06 13:45:33 +0400

    Recursively compute "n" - the Fibonacci number, provided that the sequence starts with "0"

--- main.cpp
+++ main.cpp
@@ -1,32 +1,16 @@
 #include <iostream> 
 using namespace std;
 
-
-int fib(int i)
+int f(int n)
 {
-    int f1 = 0;
-    int f2 = 1;
-    int fn;
-    if (i < 1) { return 0; }
-    if (i == 1) { cout << "0 1\n"; }
-    if (i > 1)
-    {
-        cout << "0 1 ";
-        for (int j = 1; j < i; j++)
-        {
-            fn = f1 + f2;
-            cout << fn << " ";
-            f1 = f2;
-            f2 = fn;
-        }
-    }
-};
+    if (n == 1 || n == 2) return (n - 1);
+    return f(n - 1) + f(n - 2);
+}
 
 int main()
 {
-    cout << "Hello world";
     int n;
-    cout << "Enter a number n = "; cin >> n;
-    cout << fib(n) << " ";
+    cout << "n="; cin >> n;
+    cout << f(n) << " ";
     cout << endl;
 }
